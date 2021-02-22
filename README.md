f0384959c14bc4b644b40a2bfa8907569af2ae54
Mikael <melkonyan2311@icloud.com>
2021-02-22 16:23:06 +0200

    Project created

--- README.md
+++
@@ -1,1 +0,0 @@
-# SwfitUI-UDF
---
+++ SwiftUI-UDF/SwiftUI_UDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UDFApp.swift
+//  SwiftUI-UDF
+//
+//  Created by Mikael Melkonyan on 22.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
